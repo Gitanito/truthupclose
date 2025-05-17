54a85b9d68f21e5b622b6730fa9ae2a7a5e63a76
Gitanito <nico.spataro@gmail.com>
2025-05-17 20:43:13 +0200

    Create grabbet.js

---
+++ grabbet.js
@@ -0,0 +1,73 @@
+let autobet = function(){
+	//window.location = window.location;
+	setTimeout(function(){
+		$('#highlights_tab_lastMinute').click();
+		setTimeout(function(){
+			$('div.t3-list-entry').each(
+				function (){
+					$('.t3-product__wrapper-outer').css("background-color","lightblue");
+					let games = [];
+					if (localStorage.getItem("bettedGames") != null) {
+						games = JSON.parse(localStorage.getItem("bettedGames"));
+					}
+
+					let line = $(this).data('eventid') + "\n" + $(this).find('.bet-group-column-1 .t3-bet-button').text();
+					const regex = /([0-9]*)[\n]{2}([0-9,]+)[\n]{2}([0-9,]+)[\n]{2}([0-9,]+)/gm;
+					
+					let m;
+					m = regex.exec(line);
+					if ( m) {
+						let b = parseFloat(m[2].replace(",","."));
+						let c = parseFloat(m[3].replace(",","."));
+						let d = parseFloat(m[4].replace(",","."));
+
+						let minimum = 2.5;
+						let minimum_draw = 3.1;
+
+						if (b >= minimum && d >= minimum && d+d >=4.9 && c >= minimum_draw) {
+							console.log(m[1]);
+							console.log(b);
+							console.log(c);
+							console.log(d);
+							console.log(b+c+d);
+							$('#event_'+m[1]).css("background-color","red");
+							let x = $(this).find('.bet-group-column-1 .t3-bet-button');
+							if(!games.includes(m[1])) {
+								games.push(m[1]);
+								x[0].click();
+								setTimeout(function(){
+									$('#singlebets_betamount').attr("data-steps", "0,1,2,3");
+									$('#betamount_plus').click();
+									setTimeout(function(){
+										//$('#placeBetButton').click();
+										setTimeout(function(){
+											//$('#commitBet').click();
+											setTimeout(function(){
+												x[2].click();
+												setTimeout(function(){
+													$('#singlebets_betamount').attr("data-steps", "0,1,2,3");
+													$('#betamount_plus').click();
+													setTimeout(function(){
+														//$('#placeBetButton').click();
+														setTimeout(function(){
+															//$('#commitBet').click();
+														}, 1000);
+													}, 1000);
+												}, 1000);
+											}, 1000);
+										}, 1000);
+									}, 1000);
+								}, 1000);
+								localStorage.setItem("bettedGames", JSON.stringify(games));
+							} else {
+								$('#event_'+m[1]).css("background-color","lightgreen");
+							}
+						}
+					}
+				}
+			)
+		}, 3000);
+	}, 5000);
+}
+window.setInterval("autobet()", 60000 * 5); // 5 Minuten
+autobet();
